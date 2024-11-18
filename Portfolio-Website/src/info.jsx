0ac2359a0d18b3dd2aa404cb5f0af379117d4e10
ARRYTHEBEAST <arjunkhamkar25@gmail.com>
2024-11-17 17:03:06 -0800

    do some shit

---
+++ Portfolio-Website/src/info.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const info = () => {
+    return(
+        <>
+            <h1>Arjun Khamkar</h1>
+            <h2>Student at University of Washington</h2>
+
+            <h3> Info</h3>
+            <p> I am into ML DL and pretty much anything that seems remotely interesting and nerdy.
+            </p>
+            <p>Contact:</p>
+            <ul>
+                <li>Email: <a href="mailto:arjunkha@uw.edu">arjunkha@uw.edu</a></li>
+                <li>GitHub: <a href="https://github.com/arrythebeast">arrythebeast</a></li>
+                <li>LinkedIn: <a href="https://linkedin.com/in/arjun-khamkar">arjun-khamkar</a></li>
+            </ul>
+        </>
+    )
+}
